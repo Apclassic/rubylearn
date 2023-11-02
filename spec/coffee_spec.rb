73427af500a512f941fe132c2ae4a6f23a6e04e9
Apclassic <brodeybriggs@hotmail.com>
2023-11-02 13:59:13 -0500

    marking work in progress with rspec

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -21,6 +21,8 @@ RSpec.describe 'A cup of coffee' do
 
     context 'with milk' do # context is just an alias for 'describe' however it should be used to modify objects
         before { coffee.add :milk }
+        it 'is light in color' # mapping out future work
+        it 'is cooler than 200 degrees Fahrenheit' # mapping out future work
 
         it 'costs $1.25' do
             expect(coffee.price).to eq(1.25)
