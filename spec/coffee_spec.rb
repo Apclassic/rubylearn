2a220b972ade1ad1ce841c2a0f0a9456aafcc658
Apclassic <brodeybriggs@hotmail.com>
2023-11-02 14:09:43 -0500

    Testing work in progress specs

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -10,6 +10,14 @@ class Coffee
     def price
         1.00
     end
+
+    def color
+        ingredients.include?(:milk) ? :light :dark
+    end
+
+    def temperature
+        ingredients.include(:milk) ? 190.0 : 205.0
+    end
 end
 
 RSpec.describe 'A cup of coffee' do
@@ -22,12 +30,10 @@ RSpec.describe 'A cup of coffee' do
     context 'with milk' do # context is just an alias for 'describe' however it should be used to modify objects
         before { coffee.add :milk }
         it 'is light in color' do 
-            pending 'Color not implemented yet'
             expect(coffee.color).to be(:light)
         end
 
         it 'is cooler than 200 degrees Fahrenheit' do 
-            pending 'Temperature not implemented yet'
             expect(coffee.temperature).to be < 200.0
         end
     
