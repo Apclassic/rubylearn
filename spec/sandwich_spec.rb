7aa81b6b3ce22bd3fb7168f839a5d5f3f31d1b94
Apclassic <brodeybriggs@hotmail.com>
2023-11-02 13:27:26 -0500

    Heres some examples of hooks,helper methods, and lets

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,7 +1,8 @@
 Sandwich = Struct.new(:taste, :toppings)
 RSpec.describe 'An ideal sanwich' do # creates an example group (set of related tests)
+    let(:sandwich) { Sandwich.new('delicious', []) } #Sharing objects with 'let'
   it 'is delicious' do # nested block - this is an "example" of sandwhich's use (individual test)
-    sandwich = Sandwich.new('delicious', [])
+    
 
     taste = sandwich.taste
 
@@ -9,9 +10,51 @@ RSpec.describe 'An ideal sanwich' do # creates an example group (set of related 
   end
 
   it 'lets me add toppings' do
-    sandwich = Sandwich.new('declicious', [])
+    
     sandwich.toppings << 'cheese'
     toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
+  end
+end
+
+Sandwich = Struct.new(:taste, :toppings)
+RSpec.describe 'An ideal sanwich' do # creates an example group (set of related tests)
+    def sandwich #Using a helper method is another way to setup our sandwich
+        @sandwich ||= Sandwich.new('declicious', [])
+    end
+  it 'is delicious' do # nested block - this is an "example" of sandwhich's use (individual test)
+    
+
+    taste = sandwich.taste
+
+    expect(taste).to eq('delicious') # expect (expectation or verifying outcome) - same as making assertions in E2E testing
+  end
+
+  it 'lets me add toppings' do
+    
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
+  end
+end
+
+Sandwich = Struct.new(:taste, :toppings)
+RSpec.describe 'An ideal sanwich' do # creates an example group (set of related tests)
+    before { @sandwich = Sandwich.new('delicious', []) } # Before hooks are another alternative to set up sandwich
+  it 'is delicious' do # nested block - this is an "example" of sandwhich's use (individual test)
+    
+
+    taste = @sandwich.taste
+
+    expect(taste).to eq('delicious') # expect (expectation or verifying outcome) - same as making assertions in E2E testing
+  end
+
+  it 'lets me add toppings' do
+    
+    @sandwich.toppings << 'cheese'
+    toppings = @sandwich.toppings
 
     expect(toppings).not_to be_empty
   end
